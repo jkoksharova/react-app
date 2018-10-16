00fcc7e1b3d7ddc8c5980c7cefb2e3d01477f513
Julia Koksharova <v-kokshy-gri@ae.com>
2018-10-16 17:39:13 +0300

    Update SideDrawer with backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Wrapper from '../../hoc/Wrapper';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Wrapper>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.content}>
-            {props.children}
-        </main>
-    </Wrapper>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    render () {
+        return (
+            <Wrapper>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </Wrapper>
+        )
+    }
+}
+
+export default Layout;
