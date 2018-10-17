ae32fc6c972be6d52521197eec4c318041bd1075
Julia Koksharova <v-kokshy-gri@ae.com>
2018-10-17 16:49:39 +0300

    Add orders route

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import Button from '../../../components/UI/Button/Button';
+// import classes from './ContactData.css'
+// import axios from "../../../axios-orders";
+// import Spinner from '../../../components/UI/Spinner/Spinner';
+import Order from '../../components/Order/Order';
+
+
+class Orders extends Component {
+    render () {
+        return (
+            <Order></Order>
+        );
+    }
+}
+
+export default Orders;
