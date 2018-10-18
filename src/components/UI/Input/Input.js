b71ac69e1f2a48dc5c4800aa70b6e86ec76a2b53
Julia Koksharova <v-kokshy-gri@ae.com>
2018-10-18 13:46:47 +0300

    Fix bugs and improve UX

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,10 +3,12 @@ import classes from './Input.css';
 
 const Input = (props) => {
     let inputElement = null;
+    let validationError = null;
     const inputClasses = [classes.InputElement];
 
-    if (props.invalid && props.shouldValidate) {
+    if (props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
+        validationError = <p className={classes.Error}>Please enter a valid value!</p>;
     }
 
     switch (props.elementType) {
@@ -50,6 +52,7 @@ const Input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 
