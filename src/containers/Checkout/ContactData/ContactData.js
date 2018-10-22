0cdc8224864ea8068c77342b9c2744195dace88b
Julia K <jkoksharova@outlook.com>
2018-10-22 22:13:56 +0300

    Add order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
-import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
@@ -106,14 +105,6 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         };
-        axios.post( '/orders.json', order )
-            .then( response => {
-                this.setState( { loading: false } );
-                this.props.history.push( '/' );
-            } )
-            .catch( error => {
-                this.setState( { loading: false } );
-            } );
     };
 
     checkValidity(value, rules) {
