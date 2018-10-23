a4b7ad1bd577f01f97287a53dfdcceea54039f73
Julia Koksharova <v-kokshy-gri@ae.com>
2018-10-23 17:19:27 +0300

    Handling purchasing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,6 +3,7 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
     checkoutCancelledHandler = () => {
@@ -16,8 +17,10 @@ class Checkout extends Component {
     render () {
         let summary = <Redirect to="/" />;
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -34,7 +37,8 @@ class Checkout extends Component {
 
 const mapStatesToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
 
