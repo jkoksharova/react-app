821e7418ed32294fc5a15300dc8f472d02bcd26d
Julia K <jkoksharova@outlook.com>
2018-10-28 17:43:20 +0300

    Clean up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -78,10 +78,8 @@ export const auth = (email, password, isSignup) => {
             url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDsdCYfhvv1STvO-ild2qu87GVNaHoSy5k';
         }
 
-        console.log(authData);
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
@@ -90,7 +88,6 @@ export const auth = (email, password, isSignup) => {
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                console.log(err);
                 dispatch(authFail(err.response.data.error));
             })
     };
